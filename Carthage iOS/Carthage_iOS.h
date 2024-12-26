b866d6a5a00017a624d41717429f3b4ce8c53cc6
Mayur Bendale <msbendale91@gmail.com>
2024-12-26 14:58:01 +0530

    Initial commit

---
+++ Carthage iOS/Carthage_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Carthage_iOS.h
+//  Carthage iOS
+//
+//  Created by Mayur Bendale on 26/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Carthage_iOS.
+FOUNDATION_EXPORT double Carthage_iOSVersionNumber;
+
+//! Project version string for Carthage_iOS.
+FOUNDATION_EXPORT const unsigned char Carthage_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage_iOS/PublicHeader.h>
+
+
